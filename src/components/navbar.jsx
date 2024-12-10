6f441fa7161308f0721f3e65931ff1e223b48633
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-12-10 13:35:33 +0100

    add links to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <NavLink className="navbar-brand" href="#">
+          <NavLink className="navbar-brand" to="#">
             Navbar
           </NavLink>
           <button
@@ -24,13 +24,18 @@ export default function Navbar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink className="nav-link" aria-current="page" href="#">
+                <NavLink className="nav-link" aria-current="page" to="/">
                   Home
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" href="#">
-                  Link
+                <NavLink className="nav-link" to="/about">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/posts">
+                  Posts
                 </NavLink>
               </li>
             </ul>
